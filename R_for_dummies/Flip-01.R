a2692ff3663a5a8da13f35e9dd7c8b4de14a0bea
Shriyaa Mittal <mittalshriyaa@gmail.com>
2017-11-17 11:14:50 -0600

    Create Flip-01.R

---
+++ R_for_dummies/Flip-01.R
@@ -0,0 +1,27 @@
+setwd("/Users/shriyaa/Desktop/CS598SS/project/check-MCMC")
+
+inputfile <- "dis-1.txt"
+outputfile <- "mydata_new.txt"
+
+R <- 35
+
+# Use the both below variables as 1 in case of actual MCMC
+start_num_1 <- 1
+start_num_2 <- 34
+
+
+mydata = read.table(inputfile,sep="\t", fill = TRUE)
+mydata= mydata[, start_num_1:35]
+mydata_new=mydata
+for ( i in start_num_1:R)
+{
+  for ( j in start_num_2:R)
+  {
+    if ( !is.na(mydata[i,j]) && runif(1) >= 0.99 && mydata[i,j] == 0 )
+      mydata_new[i,j] = 1
+      else if (!is.na(mydata[i,j]) && runif(1) >= 0.99 && mydata[i,j] == 1)
+      mydata_new[i,j] = 0
+  }
+}
+
+write.table(mydata_new, outputfile, sep="\t", col.names=FALSE, row.names = FALSE)
