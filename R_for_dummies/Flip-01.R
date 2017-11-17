0c93e58276ffb88e1def0474f9b9fc665ea80eb7
Shriyaa Mittal <mittalshriyaa@gmail.com>
2017-11-17 11:33:04 -0600

    Update Flip-01.R

--- R_for_dummies/Flip-01.R
+++ R_for_dummies/Flip-01.R
@@ -6,16 +6,16 @@ outputfile <- "mydata_new.txt"
 R <- 35
 
 # Use the both below variables as 1 in case of actual MCMC
-num_rows <- 1
-num_cols <- 34
+num_rows <- R
+num_cols <- R
 
 
 mydata = read.table(inputfile,sep="\t", fill = TRUE)
 mydata= mydata[, start_num_1:35]
 mydata_new=mydata
-for ( i in start_num_1:R)
+for ( i in R-num_cols:R)
 {
-  for ( j in start_num_2:R)
+  for ( j in R-num_rows:R)
   {
     if ( !is.na(mydata[i,j]) && runif(1) >= 0.99 && mydata[i,j] == 0 )
       mydata_new[i,j] = 1
