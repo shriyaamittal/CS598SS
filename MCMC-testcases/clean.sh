5718378d88c293ff19a946e7923a899f437fdede
Shriyaa Mittal <mittalshriyaa@gmail.com>
2017-11-28 22:42:29 -0600

    Update clean.sh

--- MCMC-testcases/clean.sh
+++ MCMC-testcases/clean.sh
@@ -1,21 +1,6 @@
-rm new-0-1-iter1*
-rm new-0-1-iter2*
-rm new-0-1-iter3*
-rm new-0-1-iter4*
-rm new-0-1-iter5*
-rm new-0-1-iter6*
-rm new-0-1-iter7*
-rm new-0-1-iter8*
-rm new-0-1-iter9*
-rm old-0-1-iter1*
-rm old-0-1-iter2*
-rm old-0-1-iter3*
-rm old-0-1-iter4*
-rm old-0-1-iter5*
-rm old-0-1-iter6*
-rm old-0-1-iter7*
-rm old-0-1-iter8*
-rm old-0-1-iter9*
+rm listDatasets
+rm new-0-1-iter*
+rm old-0-1-iter*
 
 rmdir data_size*
 rm -r data_size*
